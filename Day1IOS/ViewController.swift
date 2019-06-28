9f060677fb55b3150b4221f5c4a330814a1aaffb
madepoguabhishek <abhishek.madepogu@gmail.com>
2019-06-27 21:30:18 -0400

    calculator

--- Day1IOS/ViewController.swift
+++ Day1IOS/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LblMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet weak var TextField2: UITextField!
+    
+    @IBOutlet weak var TextField: UITextField!
+    
+    @IBAction func BtnAdd(_ sender: UIButton) {
+        LblMessage.text=String(Int(TextField.text!)!+Int(TextField2.text!)!)
+        
+    }
+    @IBAction func BtnSub(_ sender: UIButton) {
+        LblMessage.text=String(Int(TextField.text!)!-Int(TextField2.text!)!)
+        
+    }
+    @IBAction func BtnMul(_ sender: UIButton) {
+        LblMessage.text=String(Int(TextField.text!)!*Int(TextField2.text!)!)
+        
+    }
+    @IBAction func BtnDiv(_ sender: UIButton) {
+        LblMessage.text=String(Int(TextField.text!)!/Int(TextField2.text!)!)
+        
+    }
 }
 
